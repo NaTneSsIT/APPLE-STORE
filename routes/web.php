c506c717438d7b27f0b50b317c703cb760c8bbce
NaTneSsIT <bestnnt2001@gmail.com>
2022-11-22 21:45:21 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',
     action : [HomeController::class,'index']
 )->name('home');
+Route::get('/shop',
+    action : [ShopController::class,'index'])->name('shop');
